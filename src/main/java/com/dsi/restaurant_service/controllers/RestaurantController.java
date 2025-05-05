c74511103f5032146c2386fc8c0135e24aa66983
eyya <dhaouieya00@gmail.com>
2025-05-05 21:45:56 +0100

    eee

--- src/main/java/com/dsi/restaurant_service/controllers/RestaurantController.java
+++ src/main/java/com/dsi/restaurant_service/controllers/RestaurantController.java
@@ -5,12 +5,14 @@ import com.dsi.restaurant_service.entity.Menu;
 import com.dsi.restaurant_service.services.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/RestaurantService")
 public class RestaurantController {
     @Autowired
     private RestaurantService restaurantService;
